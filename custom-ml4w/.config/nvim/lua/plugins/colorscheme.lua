392e4783202cf91fd51304ea657a659aa9975f7c
Ian Poon <79706083+extremefattypunch@users.noreply.github.com>
2025-08-07 23:19:13 +0200

    bew

--- custom-ml4w/.config/nvim/lua/plugins/colorscheme.lua
+++ custom-ml4w/.config/nvim/lua/plugins/colorscheme.lua
@@ -7,6 +7,20 @@
 
 return {
   {
+    "zenbones-theme/zenbones.nvim",
+    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    dependencies = "rktjmp/lush.nvim",
+    lazy = false,
+    priority = 1000,
+    -- you can set set configuration options here
+    -- config = function()
+    --   vim.g.zenbones_darken_comments = 45
+    --   vim.cmd.colorscheme("zenbones")
+    -- end,
+  },
+  {
     "lalitmee/cobalt2.nvim",
     event = { "ColorSchemePre" }, -- if you want to lazy load
     dependencies = { "tjdevries/colorbuddy.nvim", tag = "v1.0.0" },
@@ -52,7 +66,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "cobalt2",
+      colorscheme = "neobones",
     },
   },
 }
